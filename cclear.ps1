182706a476a4ca5cb34f020c6216bcaa73fb1991
Kel Byers <ruferto@gmail.com>
2024-03-23 16:49:35 -0500

    make cclear a function module

--- cclear.ps1
+++ cclear.ps1
@@ -1,51 +1,51 @@
-$OD = (Get-Location).Path
-if ($PWD -like "*The Sims 3*") {
+function Clear-Sims3Cache {
+    if ($PWD -like "*The Sims 3*") {
 
-    # remove the *Cache package files:
-    # CASPartCache: cache for CAS Parts (like hair and clothes)
-    # compositorCache: cache for compositors, which are used to render Sims
-    # scriptCache: cache for scripts, which are used to make Sims do things
-    # simCompositorCache: cache for Sim compositors
-    # socialCache: cache for social interactions, like Sims talking to each other
-    # Thumbnails\CASThumbnails.package: thumbnails for CAS parts
-    # Thumbnails\ObjectThumbnails.package: thumbnails for game objects
-    @(
-        "CASPartCache.package", 
-        "compositorCache.package", 
-        "scriptCache.package", 
-        "simCompositorCache.package", 
-        "socialCache.package", 
-        "Thumbnails\CASThumbnails.package", 
-        "Thumbnails\ObjectThumbnails.package"
-    ) | ForEach-Object { Remove-Item "$_\*" }
+        # remove the *Cache package files:
+        # CASPartCache: cache for CAS Parts (like hair and clothes)
+        # compositorCache: cache for compositors, which are used to render Sims
+        # scriptCache: cache for scripts, which are used to make Sims do things
+        # simCompositorCache: cache for Sim compositors
+        # socialCache: cache for social interactions, like Sims talking to each other
+        # Thumbnails\CASThumbnails.package: thumbnails for CAS parts
+        # Thumbnails\ObjectThumbnails.package: thumbnails for game objects
+        @(
+            "CASPartCache.package", 
+            "compositorCache.package", 
+            "scriptCache.package", 
+            "simCompositorCache.package", 
+            "socialCache.package", 
+            "Thumbnails\CASThumbnails.package", 
+            "Thumbnails\ObjectThumbnails.package"
+        ) | ForEach-Object {
+            Remove-Item "$_\*" -ErrorAction SilentlyContinue
+        }
 
-    # remove temp package files in DCBackup, but do not remove ccmerged.package
-    @(Get-ChildItem DCBackup -Filter "0x*.package" `
-        | Where-Object { $_.Name -ne "ccmerged.package" }) `
-    | Remove-Item
+        # remove temp package files in DCBackup, but do not remove ccmerged.package
+        @(Get-ChildItem DCBackup -Filter "0x*.package" `
+            | Where-Object { $_.Name -ne "ccmerged.package" }) `
+        | Remove-Item -ErrorAction SilentlyContinue
 
-    # clean up script dumps and crash dumps
-    @("xcpt*.*", "ScriptError_*.*", "KW*.xml") | ForEach-Object { Remove-Item "$_\*" }
+        # clean up script dumps and crash dumps
+        @("xcpt*.*", "ScriptError_*.*", "KW*.xml") `
+        | ForEach-Object { Remove-Item "$_\*" -ErrorAction SilentlyContinue }
 
-    # clean up FeaturedItems (ads for Store Content)
-    @(Get-ChildItem FeaturedItems -Recurse) | Remove-Item
+        # clean up FeaturedItems (ads for Store Content)
+        @(Get-ChildItem FeaturedItems -Recurse) | Remove-Item -ErrorAction SilentlyContinue
 
-    # make sure FeaturedItems cannot have new content downloaded
-    # make sure ACLs are clean before denying
-    Set-Acl -Path FeaturedItems -AclObject (Get-Acl -Path FeaturedItems) -Verbose
-    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule $env:UserName, "ReadAndExecute", "None", "None", "Deny"
-    $acl = Get-Acl FeaturedItems
-    $acl.SetAccessRule($rule)
-    Set-Acl FeaturedItems $acl
+        # make sure FeaturedItems cannot have new content downloaded
+        # make sure ACLs are clean before denying: WD - write data/add file
+        icacls FeaturedItems /reset
+        $username = $env:UserName
+        icacls FeaturedItems /deny "${username}:(OI)(CI)(WD)"
 
-    # clean up DCCache TMP files
-    @(Get-ChildItem DCCache -Filter "*.tmp") | Remove-Item
+        # clean up DCCache TMP files
+        @(Get-ChildItem DCCache -Filter "*.tmp") | Remove-Item -ErrorAction SilentlyContinue
 
+    }
+    else {
+        Write-Host "*\The Sims 3\ not found"
+        exit 1
+    }    
 }
-else {
-    Write-Host "*\The Sims 3\ not found"
-    Read-Host "Press Enter to continue"
-}
 
-Set-Location $OD
-Read-Host "Press Enter to exit"
