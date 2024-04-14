6f5cb5ffd6924d2ea0a11f40e825139f5acccb82
Kel Byers <ruferto@gmail.com>
2024-04-14 16:25:06 -0500

    Debug icacls (#3)
    * tell user cclear actions for icacls
    
    * pause after clean, before launch

--- launch.ps1
+++ launch.ps1
@@ -3,20 +3,23 @@
 # source cclear.ps1 from $PSScriptRoot
 . $PSScriptRoot\cclear.ps1
 
-# the user's game settings directory is at "C:\Users\kel\Documents\Electronic Arts\The Sims 3"
 
 try {
-    Push-Location "C:\Users\kel\Documents\Electronic Arts\The Sims 3"
+    # the user's game settings directory is at "$env:HOME\Documents\Electronic Arts\The Sims 3"
+    Push-Location "$env:HOME\Documents\Electronic Arts\The Sims 3"
     # clear the game's cache
     Clear-Sims3Cache
-    
+
     # update The Sims 3's options.ini file
     SetupNvidia Options.ini
-    
-    # the launch command is at "C:\Program Files (x86)\Electronic Arts\The Sims 3\Game\Bin\Sims3Launcher.exe"
-    # launch the game, passing the command line arguments
+
 }
 finally {
     Pop-Location
+    Write-Host -NoNewLine 'Press any key to continue...';
+    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 }
+
+# the launch command is at "C:\Program Files (x86)\Electronic Arts\The Sims 3\Game\Bin\Sims3Launcher.exe"
+# launch the game, passing the command line arguments
 & "C:\Program Files (x86)\Electronic Arts\The Sims 3\Game\Bin\Sims3Launcher.exe" $args
